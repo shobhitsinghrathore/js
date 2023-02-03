ec2bea71fde7a817a673e4a7301221caabe633e5
Shobhit Singh <124371682+shobhitsinghrathore@users.noreply.github.com>
2023-02-03 19:41:40 +0530

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+let name = "John";
+let age = 30;
+console.log(`My name is ${name} and I am ${age} years old.`);
+
+var college = "kiit";
+var department = "llb";
+console.log(`my college name is ${college} and i am learning ${department}`);
+
+let jee = "abhishek";
+let jeeadvance = "mohan";
+console.log(`1st topper is ${jee} and last ranker is ${jeeadvance}`);
+
+if(name) {
+    console.log(`unefined`)
+  }
